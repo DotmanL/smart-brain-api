5973bfa9381e4d2e88f95021b3da152143773207
DotmanL <60046089+DotmanL@users.noreply.github.com>
2020-02-28 22:12:05 +0100

    adding db connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image= require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-        host: 'postgresql-acute-95100',
-        user: 'postgres',
-        password: 'test',
-        database: 'smart'
+        host:process.env.DATABASE_URL,
+        ssl:true,
     }
 });
 
